97dc6a77c953e13cc710014a3b7173ea211b67cd
fullstack412 <dt897867@gmail.com>
2020-04-29 19:11:42 -0700

    add test landing page

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('home', 'HomeController@index');
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
